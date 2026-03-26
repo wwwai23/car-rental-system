c8424cf5e45d6fa5923c7ac856839cd1853c5a09
winwinwai-gic <winwinwai@gicjp.com>
2026-03-26 13:53:08 +0630

    feat: configure supabase client and async storage

---
+++ app/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+const index = () => {
+  return <Text className="text-red-500 font-bold">Hello</Text>;
+};
+
+export default index;
